4a91e854d2f25fda600e0e152be051bdab206bdd
Joshua Feliciano <joshfeli613@gmail.com>
2022-07-29 21:42:02 +0000

    Somewhere in the middle of figuring out a db migration and HTML formatting for descriptions
    Co-authored-by: Uju Uzoagu <ujuuzoagu@yahoo.com>
    Co-authored-by: Lemson Mureya <lmureya@conncoll.edu>

--- static/assets/js/getDelete.js
+++ static/assets/js/getDelete.js
@@ -1,19 +1,21 @@
 function getDelete(job_id) {
-  var title = document.getElementById("job_title_"+job_id).innerHTML;
-  var c_name = document.getElementById("company_name_"+job_id).innerHTML;
-  var job_location = document.getElementById("job_location_"+job_id).innerHTML;
-  var job_desciption = document.getElementById("job_description_"+job_id).innerHTML;
+  var title = document.getElementById("job_title_"+job_id).innerText;
+  // var c_name = document.getElementById("company_name_"+job_id).innerText;
+  // var job_location = document.getElementById("job_location_"+job_id).innerText;
+  // var job_description = document.getElementById("job_description_"+job_id).innerText;
 
   $.ajax({
     type: "POST",
     url: "delete_job",
     data: JSON.stringify({
-       "job_title": title,
-       "company_name": c_name,
-       "location": job_location,
-       "description": job_desciption,
+      "job_id": job_id,
+      "job_title": title//,
+      // "company_name": c_name,
+      // "location": job_location,
+      // "description": job_description,
     }),
     contentType: "application/json",
     dataType: 'json'
   });
+
 }
