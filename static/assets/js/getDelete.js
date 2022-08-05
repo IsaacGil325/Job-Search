8579c84a587e517b19a0954eb8d7d6ce4c53bb04
Joshua Feliciano <joshfeli613@gmail.com>
2022-08-05 05:21:18 +0000

    Some small touch-ups for final presentation
    Co-authored-by: Uju Uzoagu <ujuuzoagu@yahoo.com>
    Co-authored-by: Lemson Mureya <lmureya@conncoll.edu>
    Co-authored-by: Isaac Gil <isaacgil325@gmail.com>

--- static/assets/js/getDelete.js
+++ static/assets/js/getDelete.js
@@ -4,16 +4,33 @@ function getDelete(job_id) {
   // var job_location = document.getElementById("job_location_"+job_id).innerText;
   // var job_description = document.getElementById("job_description_"+job_id).innerText;
 
-  console.log('job id:', job_id);
+  let theTable = document.querySelector("tbody");
+  let theRow = document.getElementById(job_id);
+  allRows = Array.from(theTable.children)
+  jobPos = allRows.indexOf(theRow) + 1;
+
+  for(let i=jobPos; i<allRows.length; i++){
+    currentRow = theTable.children[i];
+    currentRowNum = parseInt(currentRow.children[0].innerText);
+    currentRow.children[0].innerText = `${currentRowNum - 1}`;
+  }
+
+  // trying to fade out the removal of a table row
+  // theRow.style.display = 'none';
+  // theRow.style.transition = 'display 1s';
+  // theRow.style.webkitTransition = 'display 1s';
+  // theRow.style.oTransition = 'display 1s';
+  // theRow.style.msTransition = 'display 1s';
+  // theRow.style.mozTransition = 'display 1s';
+  theRow.remove();
+
+  // console.log('job id:', job_id);
   $.ajax({
-    type: "POST",
+    type: "DELETE",
     url: "delete_job",
     data: JSON.stringify({
-      "job_id": job_id,
-      "job_title": title//,
-      // "company_name": c_name,
-      // "location": job_location,
-      // "description": job_description,
+      "job_id": job_id//,
+      // "job_title": title // had it for debugging purposes on the backend to print the title
     }),
     contentType: "application/json",
     dataType: 'json'
--- static/assets/js/getResults.js
+++ static/assets/js/getResults.js
@@ -1,18 +1,18 @@
 function getResults(job_id) {
-  console.log('from getResults:')
+  ?console.log('from getResults:')
   
   var title = document.getElementById("job_title_"+job_id).innerText;
   var c_name = document.getElementById("company_name_"+job_id).innerText;
   var job_location = document.getElementById("job_location_"+job_id).innerText;
   var job_description = document.getElementById("job_description_"+job_id).innerText;
-  console.log('job id:', job_id)
-  console.log(job_description)
+  // console.log('job id:', job_id)
+  // console.log(job_description)
   // console.log(title)
   // console.log(c_name)
   // console.log(job_location)
   // console.log(job_desciption)
   $.ajax({
-    type: "POST",
+    type: "PUT",
     url: "saved_jobs",
     data: JSON.stringify({
       "job_id": job_id,
